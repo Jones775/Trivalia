c276b94e23778587e4a95f71dca2c6833780d2ea
Jones775 <aljosha.krueger@gmail.com>
2023-11-23 18:56:24 +0100

    Added Custom Classes file

---
+++ Classes.py
@@ -0,0 +1,21 @@
+import time
+import random
+from Enemy_Class import *
+from Item_Class import *
+
+
+
+#Classes
+
+#Here you can create custom classes
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    print("Bitte oeffne die Datei \"Spiel.py\"")
+    time.sleep(10)
+
