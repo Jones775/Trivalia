9fe7be71f56e52c32c84f561c9c851a9fedb1e70
Jones775 <aljosha.krueger@gmail.com>
2023-11-23 19:14:29 +0100

    I thought I would need this file, but I was wrong, it is completely unnecessary

---
+++ Variables.py
@@ -0,0 +1,15 @@
+from Classes import *
+from Player_Class import *
+from Functions import *
+
+
+# Variables
+
+
+
+
+if __name__ == "__main__":
+
+    print("Bitte oeffne die Datei \"Spiel.py\"")
+    time.sleep(10)
+
