b2304ea73a1a6b80e07fe99846984df4edf0e820
Jones775 <aljosha.krueger@gmail.com>
2023-11-23 18:50:57 +0100

    Main file changed

--- Spiel.py
+++ Spiel.py
@@ -1,130 +1,104 @@
 import time
+import sys
 from Classes import *
 from Enemy_Class import *
 from Player_Class import *
-from Functions import *
-from Variables import *
-import sys
 from Item_Class import *
 from NPC_Class import *
+from Locations_Class import *
+from Quest_Class import *
 
 
 
 
+
+time.sleep(3)
+print("Herzlich Willkommen zu Trivalia")
+time.sleep(2)
 player_name = input_player_name()
-show_introduction(player_name)
-#difficulty = input_difficulty()
+time.sleep(2)
 
-#Create Locations
-
-atyr = Location("atyr", [], [])
-customs_house = Location("zollhaus", [], [])
-small_village = Location("kleines dorf", [], [])
-starting_point = Location("hütte von "+player_name, [], [])
-spruce_forest = Location("fichtenwald", [Wolf], [])
-devils_reef = Location("teufelsriff", [], [])
-
-
-forest_crossing = Location("waldkreuzung", [], [])
-river_mouth = Location("mündung", [], [])
-seagull_rock = Location("möwenfelsen", [], [])
-sentry = Location("wachposten", [], [])
-stone_cliffs = Location("steinklippen", [], [])
-yerin = Location("yerin", [], [])
-
-old_pirate_island = Special_Location("alte pirateninsel", [], [])
-port_of_atyr = Special_Location("hafen von atyr", [], [])
-ferry = Special_Location("fährhäuschen1", [], [])
-ferry2 = Special_Location("fährhäuschen2", [], [])
+while True:
+    print("Moechtest du dir das Tutorial anschauen?")
+    print("1: Ja")
+    print("2: Nein")
+    inp = input()
+    if inp == "1":
+        show_tutorial()
+        break
+    elif inp == "2":
+        print()
+        break
+    else:
+        print("Bitte gib 1 oder 2 ein")
 
 
-
-
-
-
-#Initialize attributes
-
-customs_house.initialize_location([], [yerin, ferry2])
-
-forest_crossing.initialize_location([], [atyr, yerin, seagull_rock])
-
-starting_point.initialize_location([Small_Hut(), Garden()], [spruce_forest, small_village, river_mouth])
-spruce_forest.initialize_location([], [ferry, starting_point])
-small_village.initialize_location([], [starting_point])
-seagull_rock.initialize_location([], [stone_cliffs, forest_crossing])
-sentry.initialize_location([], [yerin])
-
-river_mouth.initialize_location([], [starting_point])
-
-stone_cliffs.initialize_location([], [atyr, port_of_atyr, seagull_rock])
-
-ferry.initialize_location([], [ferry2, spruce_forest])
-
-ferry2.initialize_location([], [ferry, customs_house])
-
-old_pirate_island.initialize_location([], [port_of_atyr])
-
-port_of_atyr.initialize_location([], [old_pirate_island, atyr, stone_cliffs])
-
-atyr.initialize_location([], [port_of_atyr,stone_cliffs, forest_crossing])
-
-yerin.initialize_location([], [customs_house, sentry, forest_crossing])
+while True:
+    print("Moechtest du dir die Vorgeschichte anhoeren?")
+    print("1: Ja")
+    print("2: Nein")
+    inp = input()
+    if inp == "1":
+        show_introduction(player_name)
+        break
+    elif inp == "2":
+        print("Dann viel Spass beim Spielen")
+        break
+    else:
+        print("Bitte gib 1 oder 2 ein")
 
 
 
-
-dolch = Small_Dagger()
-rüstung = Clothes()
-armor2 = Leather_Armor()
 
 
 #Create Player
-player1 = Player(player_name, 80, [], starting_point, dolch, 40, rüstung, 50)
+player1 = Player(player_name, 80, [Apple(), Potato()], starting_point, Player_Hut(), Small_Dagger(), 20, Clothes(), 50)
 
 
 
-player1.inventory.append(Carrot())
-player1.inventory.append(Pumpkin())
-player1.inventory.append(Chicken_Soup())
-player1.inventory.append(Roast_Beef())
-player1.print_player_info()
-
-wolf = Wolf()
-wolf2 = Wolf()
-schwert = Close_Combat_Weapon("Schwert", 50)
-
-apfel = Apple()
-apfel1 = Apple()
-stock2 = Item("Stock")
-
-stock = Item("Stock")
-
-player1.inventory.append(apfel)
-
-player1.add_item_to_inventory(schwert)
-player1.add_item_to_inventory(apfel)
-player1.add_item_to_inventory(armor2)
 
 
-player1.show_inventory(False)
-player1.print_player_info()
 
-npc = Farmer()
-npc.show_dialogue(player1)
 
-load_save_file("speichern", player1)
+
+print("Um das Spiel zu beginnen, tippe einen Befehl ein: ")
 #Game Loop
 while True:
 
+    Quest.check_all_conditions()
 
     player1.hunger -= 1
 
+    if player1.health < player1.max_health:
+        player1.heal(5)
+
+    #Main input
     temp = input().lower().strip()
+
 
     if temp == "info":
         get_info()
     elif temp == "ende":
-        quit()
+        while True:
+            print("Sicher, dass du nicht noch speichern willst?")
+            print("1: Ja")
+            print("2: Nein")
+            inp = input()
+            if inp == "1":
+                quit()
+            elif inp == "2":
+                print("Dann lege mit \"speichern\" einen Speicherstand an")
+                break
+            else:
+                print()
+
+
+    elif player1.health <= 0:
+        player1.die()
+
+    elif "sprechen" in temp and "(" in temp:
+        prompt = get_prompt(temp)
+        player1.speak_to_npc(prompt[1])
 
     elif "reisen" in temp and "(" in temp:
         print()
@@ -134,24 +108,27 @@ while True:
                 player1.change_location(location)
                 break
         else:
-            if prompt[1] in Location.locations:
+            if prompt[1].lower().strip() in Location.generate_string_array_of_locations():
                 print("Dieser Ort ist zu weit entfernt")
             elif prompt[1] == player1.location.name:
                 print("Du befindest dich bereits in "+first_letter_uppercase(player1.location.name))
             else:
-                print(player1.name+" kennt diesen Ort nicht")
+                print(f"{player1.name} kennt diesen Ort nicht")
 
-    elif "waffewählen" in temp and "(" in temp:
+    elif "waffewaehlen" in temp and "(" in temp:
         prompt = get_prompt(temp)
         player1.change_main_weapon(prompt[1])
 
-    elif "rüstungwählen" in temp and "(" in temp:
+    elif "ruestungwaehlen" in temp and "(" in temp:
         prompt = get_prompt(temp)
         player1.change_armor(prompt[1])
 
     elif "aufheben" in temp and "(" in temp:
         prompt = get_prompt(temp)
         player1.add_item_to_inventory(prompt[1])
+
+    elif temp == "quest":
+        Quest.show_all_quests()
 
     elif "ablegen" in temp and "(" in temp:
         prompt = get_prompt(temp)
@@ -167,13 +144,37 @@ while True:
         show_map()
 
     elif temp == "speichern":
-
-        temp = input("Wie möchtest du deinen Speicherstand nennen? ")
-        if " " in temp:
+        print("Wie möchtest du deinen Speicherstand nennen? ")
+        print("Wenn du doch nicht speichern möchtest, tippe \"abbrechen\"")
+        temp = input()
+        if temp.lower().strip() == "abbrechen":
+            print("Es wurde kein Speicherstand erstellt")
+        elif " " in temp:
             print("Du darfst keine Leerzeichen verwenden")
             temp = input("Wie möchtest du deinen Speicherstand nennen? ")
         else:
             create_save_file(temp, player1)
+
+    elif temp == "laden":
+        savefiles = show_save_files()
+        print("Welchen Speicherstand möchtest du laden?")
+        print("Wenn du keinen Speicherstand laden möchtest, tippe \"abbrechen\"")
+        temp = input()
+        if temp.lower().strip() == "abbrechen":
+            print("Es wurde kein Speicherstand geladen")
+        elif temp in savefiles:
+            temp = "Save_Files\\"+temp
+
+            variables = []
+            values = []
+            for variable in list(locals().keys()):
+                variables.append(variable)
+            for value in list(locals().values()):
+                values.append(value)
+
+            load_save_file(temp, player1, variables, values)
+        else:
+            print("Du kannst diesen Speicherstand nicht laden")
 
     elif "essen" in temp and "(" in temp:
         prompt = get_prompt(temp)
@@ -182,9 +183,39 @@ while True:
     elif temp == "erkunden":
         player1.explore_location()
 
+    elif temp == "umschauen":
+        player1.look_around()
+
+
+
+    elif "gehen" in temp and "(" in temp:
+        prompt = get_prompt(temp)
+        player1.change_subarea(prompt[1])
+
+    elif "oeffnen" in temp and "(" in temp:
+        prompt = get_prompt(temp)
+        player1.open_chest(prompt[1])
+
+    elif "angreifen" in temp and "(" in temp:
+        prompt = get_prompt(temp)
+        player1.attack(prompt[1], player1.main_weapon)
+
+    elif "lexikon" in temp and "(" in temp:
+        prompt = get_prompt(temp)
+        print("Diese Funktion ist momentan noch nicht implementiert")
+        #TODO pls implement
+        #get_item_info(prompt[1])
+
+
+    elif "trinken" in temp and "(" in temp:
+        prompt = get_prompt(temp)
+        player1.drink(prompt[1], Location.locations)
+
+
     else:
-        print(player1.name+ " kann dich nicht verstehen\nVersuche es mit einem anderen Befehl\nFür eine Übersicht, gib info ein")
+        print(f"{player1.name} kann dich nicht verstehen\nVersuche es mit einem anderen Befehl\nFür eine Übersicht, gib info ein")
 
     if player1.hunger <= 0:
-        print(player1.name," ist am Verhungern")
+        print(f"{player1.name} ist am Verhungern")
         player1.take_damage(15)
+
